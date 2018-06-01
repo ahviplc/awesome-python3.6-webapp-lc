bc5de36fe2ad797b3a0e2d1e3c25cf19ed5eb61c
ahviplc <ahlc@sina.cn>
2018-06-01 18:17:05 +0800

    day3 day4 Ok !已测试，成功建表数据！TestUser.py Test for user!2018年6月1日18:15:26

---
+++ www/TestUser.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Test for user
+2018年6月1日18:09:46
+'''
+
+import sys
+import orm, asyncio
+from models import User, Blog, Comment
+
+
+def test(loop):
+    yield from orm.create_pool(loop=loop, user='root', password='root', db='awesome')
+    u = User(name='LC', email='ahlc@sina.cn', passwd='LC123', image='about:blank')
+    yield from u.save()
+
+
+if __name__ == '__main__':
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait([test(loop)]))
+    loop.close()
+    if loop.is_closed():
+        sys.exit(0)
